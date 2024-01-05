d01e19ac71f67d44be0185a4ac75cf3c6785a9b8
Fabien Lestrat <129498777+Fables7@users.noreply.github.com>
2024-01-05 11:45:19 +0000

    Added separator from shadnc and added it to navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -2,14 +2,16 @@ import logo from "@/assets/audiophile-logo.svg";
 import ShoppingCart from "@/assets/shopping-cart.svg";
 import Image from "next/image";
 import NavMenu from "./NavMenu";
+import { Separator } from "@/components/ui/separator";
 
 const NavBar = () => {
   return (
     <div className="bg-black h-[90px] w-full flex items-center justify-center ">
-      <div className="w-[1110px] flex  justify-between">
+      <div className="w-[1110px] flex  justify-between relative h-full">
         <Image src={logo} alt={"logo"} height={25} />
         <NavMenu />
-        <Image src={ShoppingCart} alt="shopping cart" />
+        <Image src={ShoppingCart} alt="shopping cart" className="ml-16" />
+        <Separator className="absolute bottom-0 bg-[#3e3e3e]" />
       </div>
     </div>
   );
